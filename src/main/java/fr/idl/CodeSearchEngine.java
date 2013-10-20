e0ea5276838cda6110d97b0240888a85abd3e12b
blckshrk <alexand.bonhom@gmail.com>
2013-10-20 19:51:21 +0200

    Nettoyage de la branche principale

--- src/main/java/fr/idl/CodeSearchEngine.java
+++ src/main/java/fr/idl/CodeSearchEngine.java
@@ -3,97 +3,115 @@ package main.java.fr.idl;
 import java.util.List;
 
 /**
- * CodeQuery specifies a list of methods for querying source code. 
- * It is designed with Java in mind but should work with many other languages as well.
- * The metamodel of code abstracts over the language (see in TypeKind for example).
- * The queries are inspired from Eclipse Java Search.
+ * CodeQuery specifies a list of methods for querying source code. It is
+ * designed with Java in mind but should work with many other languages as well.
+ * The metamodel of code abstracts over the language (see in TypeKind for
+ * example). The queries are inspired from Eclipse Java Search.
  * 
- * The code query engine can either:
- *  - works in one-shot mode (see  {@link CodeSearchEngineFile}
- *  - loads and pre-computes some data, in this case T may represent your own optimized data structure
+ * The code query engine can either: - works in one-shot mode (see
+ * {@link CodeSearchEngineFile} - loads and pre-computes some data, in this case
+ * T may represent your own optimized data structure
  * 
  * @author Martin Monperrus <martin.monperrus@univ-lille1.fr>
- *
+ * 
  */
 public interface CodeSearchEngine<T> {
-  enum TypeKind {
-    CLASS, INTERFACE, ENUM, PRIMITIVE, EXCEPTION, ANNOTATION
-  }
-  
-  interface Location {
-	/** returns a path of the file containing a code element.
-	 * E.g. "./foo/Toto.java"
+	enum TypeKind {
+		CLASS, INTERFACE, ENUM, PRIMITIVE, EXCEPTION, ANNOTATION
+	}
+
+	interface Location {
+		/**
+		 * returns a path of the file containing a code element. E.g.
+		 * "./foo/Toto.java"
+		 */
+		String getFilePath();
+
+		int getLineNumber(); // optional
+	}
+
+	interface Type {
+		String getName();
+
+		String getFullyQualifiedPackageName();
+
+		TypeKind getKind(); // optional
+
+		Location getDeclaration();
+	}
+
+	interface Member {
+		/** returns the class that defines this method */
+		Type getDeclaringType();
+
+		/** returns the class of the field or the returned object */
+		Type getType();
+
+		/** returns the name of this member (e.g. the field name) */
+		String getName();
+	}
+
+	interface Field extends Member {
+	}
+
+	interface Method extends Member {
+		List<Type> getParamaters();
+	}
+
+	/** returns the type (and its location through getLocation) of type typeName */
+	Type findType(String typeName, T data);
+
+	/** returns all subtypes of type typeName */
+	List<Type> findSubTypesOf(String typeName, T data);
+
+	/** returns all fields typed with typeName */
+	List<Field> findFieldsTypedWith(String typeName, T data);
+
+	/** returns all read accesses of the field given as parameter */
+	List<Location> findAllReadAccessesOf(Field field, T data);
+
+	/**
+	 * returns all write accesses of the field given as parameter (this.foo =
+	 * ... )
 	 */
-    String getFilePath();
-    int getLineNumber(); // optional 
-  }
-  
-  interface Type {	  
-    String getName();
-    String getFullyQualifiedPackageName();
-    TypeKind getKind();
-    Location getDeclaration();
-  }
-  
-  interface Member {
-    Type getType();
-    String getName();
-  }
+	List<Location> findAllWriteAccessesOf(Field field, T data);
 
-  interface Field extends Member {}
+	/**
+	 * returns all methods of typeName (does not consider the inherited methods)
+	 */
+	List<Method> findMethodsOf(String typeName, T data);
 
-  interface Method extends Member {
-    List<Type> getParamaters();
-  }
+	/** returns all methods returning typeName */
+	List<Method> findMethodsReturning(String typeName, T data);
 
-  /** returns the type (and its location through getLocation) of class className */
-  Type findType(String className, T data);
-  
-  /** returns all subclasses of class className */
-  List<Type> findSubTypesOf(String className, T data);
-  
-  /** returns all fields typed with className */
-  List<Field> findFieldsTypedWith(String className, T data);
+	/** returns all methods using typeName as parameter */
+	List<Method> findMethodsTakingAsParameter(String typeName, T data);
 
-  /** returns all read accesses of the field given as parameter */
-  List<Location> findAllReadAccessesOf(Field field, T data);
+	/** returns all methods called methodName */
+	List<Method> findMethodsCalled(String methodName, T data);
 
-  /** returns all write accesses of the field given as parameter (this.foo = ... ) */
-  List<Location> findAllWriteAccessesOf(Field field, T data);
+	/** returns all methods overriding the given method */
+	List<Method> findOverridingMethodsOf(Method method, T data);
 
-  /** returns all methods of className (does not consider the inherited methods) */
-  List<Method> findMethodsOf(String className, T data);
+	/**
+	 * returns all locations where there is an instance creation of typeName:
+	 * new X()
+	 */
+	List<Location> findNewOf(String className, T data);
 
-  /** returns all methods returning className */
-  List<Method> findMethodsReturning(String className, T data);
+	/** returns all locations where there is a cast to typeName */
+	List<Location> findCastsTo(String typeName, T data);
 
-  /** returns all methods using className as parameter */
-  List<Method> findMethodsTakingAsParameter(String className, T data);
+	/** returns all locations where there is an instanceof check to typeName */
+	List<Location> findInstanceOf(String typeName, T data);
 
-  /** returns all methods called methodName */
-  List<Method> findMethodsCalled(String methodName, T data);
+	/** returns all methods throwing this exception */
+	List<Method> findMethodsThrowing(String exceptionName, T data);
 
-  /** returns all methods overriding method methodName that is in className */
-  List<Method> findOverridingMethodsOf(Method method, T data);
-  
-  /** returns all locations where there is an instance creation of className: new X() */
-  List<Location> findNewOf(String className, T data);
+	/** returns all locations where there is a cast to className */
+	List<Location> findCatchOf(String exceptionName, T data);
 
-  /** returns all locations where there is a cast to className */
-  List<Location> findCastsTo(String className, T data);
-
-  /** returns all locations where there is an instanceof check to className */
-  List<Location> findInstanceOf(String className, T data);
-  
-  /** returns all methods throwing this exception */
-  List<Method> findMethodsThrowing(String exceptionName, T data);
-  
-  /** returns all locations where there is a cast to className */
-  List<Location> findCatchOf(String exceptionName, T data);
-
-  /** returns all classes annotated with annotationName */
-  List<Type> findClassesAnnotatedWith(String annotationName, T data);
+	/** returns all classes annotated with annotationName */
+	List<Type> findClassesAnnotatedWith(String annotationName, T data);
 
 }
-
-
--- src/main/java/fr/idl/Main.java
+++ src/main/java/fr/idl/Main.java
@@ -1,35 +1,20 @@
 package main.java.fr.idl;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
-import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 public class Main {
 
 	public static void main(String[] args) throws XPathExpressionException {
-		
+
 		if (args.length != 1) {
 			System.err.println("Usage : cse path/to/file.xml");
 			System.exit(-1);
 		}
-		
+
 		// input source
 		InputSource source = new InputSource(args[0]);
-	
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		XPathExpression query = (XPathExpression) xpath.compile("//function_decl/name/text()");
-		
-		NodeList result = (NodeList) query.evaluate(source, XPathConstants.NODESET);
-		
-		System.out.println("Resultat: ");
-		for (int i = 0; i < result.getLength(); i++) {
-			System.out.println(result.item(i));
-		}
 	}
 
 }
