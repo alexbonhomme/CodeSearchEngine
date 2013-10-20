e060c861513a3f66aa39f8f2337b4a83fc25c015
Tony Tran <ttran59@gmail.com>
2013-10-20 19:24:39 +0200

    Changement des packages

--- src/main/java/fr/idl/App.java
+++ src/main/java/fr/idl/App.java
@@ -1,4 +1,4 @@
-package fr.myParser;
+package main.java.fr.idl;
 
 import java.util.Calendar;
 
--- src/main/java/fr/idl/MyParserXML.java
+++ src/main/java/fr/idl/MyParserXML.java
@@ -1,4 +1,4 @@
-package fr.myParser;
+package main.java.fr.idl;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
