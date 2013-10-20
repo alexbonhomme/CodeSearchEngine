9deec6418c84efd7e63d84d50565ea774acf3b3d
MajorShepard <julien.duribreux@gmail.com>
2013-10-20 21:28:08 +0200

    Modif. main

--- src/main/java/fr/idl/Main.java
+++ src/main/java/fr/idl/Main.java
@@ -6,15 +6,16 @@ import org.xml.sax.InputSource;
 
 public class Main {
 
-	public static void main(String[] args) throws XPathExpressionException {
+  public static void main(String[] args) throws XPathExpressionException {
 
-		if (args.length != 1) {
-			System.err.println("Usage : cse path/to/file.xml");
-			System.exit(-1);
-		}
+    if (args.length != 1) {
+      System.err.println("Usage : cse path/to/file.xml");
+      System.exit(-1);
+    }
 
-		// input source
-		InputSource source = new InputSource(args[0]);
-	}
+    // input source
+    InputSource source = new InputSource(args[0]);
+
+  }
 
 }
